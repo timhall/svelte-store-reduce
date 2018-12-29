d58c740455800dd1ca88787be35362d75f99931b
Tim Hall <tim.hall.engr@gmail.com>
2018-12-29 18:15:59 -0500

    Add reduce functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import { writable } from 'svelte/store';
+
 export function reduce(reducer, initial) {
-  //
+  const state = writable(initial);
+
+  const subscribe = run => state.subscribe(run);
+  const dispatch = action => {
+    state.update(value => reducer(value, action));
+  };
+
+  return { subscribe, dispatch };
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,31 @@
 import test from 'ava';
+import { reduce } from '../';
 
-test('should reduce', t => {
-  t.pass();
+test('should dispatch and subscribe', t => {
+  const initial = { count: 0 };
+  const state = reduce((state, action) => {
+    switch (action.type) {
+      case 'reset':
+        return initial;
+      case 'increment':
+        return { count: state.count + 1 };
+      case 'decrement':
+        return { count: state.count - 1 };
+      default:
+        return state;
+    }
+  }, initial);
+
+  const values = [];
+  state.subscribe(value => {
+    values.push(value);
+  });
+
+  state.dispatch({ type: 'increment' });
+  state.dispatch({ type: 'increment' });
+  state.dispatch({ type: 'decrement' });
+  state.dispatch({});
+  state.dispatch({ type: 'reset' });
+
+  t.snapshot(values);
 });
